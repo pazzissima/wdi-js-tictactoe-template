10f1b96163ebb8c93f54e2401532ccd3725aaf8a
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2014-03-15 19:34:28 -0700

    Teams angularized, everything is working!

--- js/new_controller.js
+++ js/new_controller.js
@@ -1,38 +1,8 @@
-var TicTacApp = angular.module("TicTacApp", []);
-function TicTacController($scope) {
+var TicTacApp = angular.module("TicTacApp", ['ngSanitize']);
+function TicTacController($scope, $sce) {
 
-  //### 'jquery' UI elements for game manipulation
-  //var game              =    // the game container
-  //var board             =    // the board  container
-
-
-
-  var game = $('#game');
-  var board = $('#board')
-  var status_indicators = $('#teams li');   // status bar container
-
-  /* var tiles = [];                         // all the "tiles" */
-
-  var players = [                         // player data
-    {
-      name:      'Ernie',
-      marker:    'X',
-      img_url:   'img/ernie.jpg',
-      indicator: $(status_indicators[0])
-    },
-    {
-      name:      'Bert',
-      marker:    '0',
-      img_url:   'img/bert.jpg',
-      indicator: $(status_indicators[1])
-    }
-  ];
-
-  var current_player;                     // player data
   var turns  = 0;                         // elapsed turns
 
-  //### There are eight winning combos, the first two are supplied.
-  //### What are the other six? Add 'em.
   var win_combos = [
     [0,1,2], [3,4,5], [6,7,8],
     [0,3,6], [1,4,7], [2,5,8],
@@ -40,83 +10,56 @@ function TicTacController($scope) {
   ];
 
   $scope.initialize = function() {
-    //### ready the board for game play
-
-    //### 1.) Create nine tiles. Each is a div, each needs to be bound to 'handle_click'.
-    //### Make sure giving each tile a unique 'id' for targeting. Find tile's 'class' in css.
-    //### Append tiles to board.
-
-    //### 2.) Make first player the current_player
-
-    //### 3.) Set up the players 'indicators' in UI
-    //### - set player image, name, marker
-    //### - set player name
-    //### - the 'current_player' has a different style (see css '.current')
-
-    //### 4.) fade in the game
-
-/* tile rendering moved into Angularified HTML
-    _(9).times(function(i) {
-      tiles.push(
-         $('<div/>')
-           .attr({'id':'tile'+i, 'class':'tile'})
-           .on('click', handle_click)
-           .appendTo(board));
-    });
-*/
+    $scope.result = {
+      img_src: null,
+      img_alt: null,
+      message: null
+    };
 
     $scope.tiles = [
-      {id: 0, player: null},
-      {id: 1, player: null},
-      {id: 2, player: null},
-      {id: 3, player: null},
-      {id: 4, player: null},
-      {id: 5, player: null},
-      {id: 6, player: null},
-      {id: 7, player: null},
-      {id: 8, player: null}
-    ]
+      {id: 0, player: null, is_combo: false},
+      {id: 1, player: null, is_combo: false},
+      {id: 2, player: null, is_combo: false},
+      {id: 3, player: null, is_combo: false},
+      {id: 4, player: null, is_combo: false},
+      {id: 5, player: null, is_combo: false},
+      {id: 6, player: null, is_combo: false},
+      {id: 7, player: null, is_combo: false},
+      {id: 8, player: null, is_combo: false}
+    ];
 
-    current_player = _.first(players);
-    var player_indicators = _.map(players, function(player) {
-      return player.indicator
-    });
-    _.each(player_indicators, function(indicator, i) {
-      var indicator = $(indicator);
-      var player = players[i];
-      $('.team', indicator).html(player.marker);
-      $('.player', indicator).html(player.name);
-      $('img', indicator).attr('src', player.img_url);
-      if (player == current_player) {
-        indicator.addClass('current');
+    $scope.players = [                         // player data
+      {
+        name:      'Ernie',
+        marker:    $sce.trustAsHtml('&times;'),
+        img_url:   'img/ernie.jpg'
+      },
+      {
+        name:      'Bert',
+        marker:    $sce.trustAsHtml('&oslash;'),
+        img_url:   'img/bert.jpg'
       }
-    });
-    game.fadeIn();
+    ];
+
+    $scope.current_player = _.first($scope.players);
+
+    $('#game').fadeIn();
   };
 
   $scope.is_active = function(tile) {
     // boolean - evaluate whether or not is 'active'
-//    return tile.hasClass('active');
     return tile.player != null;
   };
   $scope.activate_tile = function(tile) {
     // activate a tile and increment the turn counter
-
-    // tile.html(current_player.marker);
-    // tile.addClass('active');
-    // tile.data('player', current_player);
-
-    tile.player = current_player;
-
+    tile.player = $scope.current_player;
     turns++;
   };
 
 
   $scope.toggle_player = function() {
     // toggle the current player and update related UI
-    current_player = players[$scope.get_current_player_index()];
-    status_indicators.removeClass('current');
-    current_player.indicator.addClass('current');
+    $scope.current_player = $scope.players[$scope.get_current_player_index()];
   };
 
   $scope.get_current_player_index = function() {
@@ -128,7 +71,7 @@ function TicTacController($scope) {
     // return the current player's positions on the board
     var current_player_board_data = [];
     _.each($scope.tiles, function(tile, i) {
-      if (tile.player == current_player) {
+      if (tile.player == $scope.current_player) {
         current_player_board_data.push(i);
       }
     });
@@ -138,14 +81,17 @@ function TicTacController($scope) {
   $scope.is_win = function() {
     // boolean - whether or not the current player's positions result in a win
     var board_data = $scope.get_board_data();
-    var match_found = false;
+
+    var win_result = false;
+
     _.each(win_combos, function(combo) {
       if (_.intersection(combo, board_data).length == combo.length) {
-        //$scope.show_combo(combo);
-        match_found = true;
+        $scope.show_combo(combo);
+        win_result = true;
       }
     });
-    return match_found;
+
+    return win_result;
   };
 
   $scope.is_tie = function() {
@@ -154,22 +100,17 @@ function TicTacController($scope) {
   };
 
   $scope.handle_win = function() {
-    // update the UI to reflect that a win has occurred.
-    /*_.each($scope.tiles, function(tile) {
-      tile.off('click');
-    });*/
     $scope.update_results({
-      img_src: current_player.img_url,
-      img_alt: current_player.name,
+      img_src: $scope.current_player.img_url,
+      img_alt: $scope.current_player.name,
       message: 'Congratulations, <span id="winner">'
-        + current_player.name
+        + $scope.current_player.name
         + '</span>!'
     });
   };
 
   $scope.handle_tie = function() {
-    // update the UI to reflect that a tie game has occurred.
-    update_results({
+    $scope.update_results({
       img_src: 'img/rubberduckie.jpg',
       img_alt: 'Rubber Duckie',
       message: 'Tie Game!'
@@ -180,48 +121,20 @@ function TicTacController($scope) {
     /* call this to update the "results" container after detecting
        a win or a tie */
 
+    $scope.result = args;
+
     var results   = $('#results');
-    var winner_el = $('h1', results);
-    var image_el  = $('.image', results);
-    var button    = $('button', results);
-    var image     = $('<img/>');
     var overlay   = $('#overlay');
-    image.attr({
-      src: args.img_src,
-      alt: args.img_alt
-    });
-    image_el.html(image);
-    winner_el.html(args.message);
-    button.on('click', $scope.new_game);
-    $scope.hide_indicators();
     setTimeout(function() {
       overlay.fadeIn(500);
       results.fadeIn(500);
-    },1000);
+    }, 1000);
   }
 
-  $scope.hide_indicators = function() {
-    // call this to hide the "status" container after detecting a win or a tie
-    status_indicators.animate({'opacity':0},2000);
-  };
-
-/*  $scope.show_combo = function(combo) {
+  $scope.show_combo = function(combo) {
     // call this to highlight the combination of tiles that resulted in a win
     _.each(combo, function(tile_index) {
-      $scope.tiles[tile_index].addClass('combo');
-    });
-  }
-*/
-
-  $scope.is_combo = function(tile_id) {
-    board_data = $scope.get_board_data;
-    _.each(win_combos, function(combo) {
-      if (_.intersection(combo, board_data).length == combo.length) {
-        _.each(combo), function(win_tile_id) {
-          if (tile_id == win_tile_id)
-            return true;
-        }
-      }
+      $scope.tiles[tile_index].is_combo = true;
     });
   }
 
